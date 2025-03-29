2b45fdbc971731d9388d1b53ab6bb2d77b01b15d
jangjun1214 <110437307+jangjun1223@users.noreply.github.com>
2025-03-29 17:13:04 +0900

    Update GameManager.cs

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -2,15 +2,33 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static GameManager gameManager;  // 싱글톤 인스턴스
+    public bool isPlayerDead = false;      // 플레이어 생사 여부
+
+    private void Awake()
     {
-        
+        // 게임 매니저 인스턴스가 하나만 존재하도록 보장
+        if (gameManager == null)
+        {
+            gameManager = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    // 예시: 플레이어가 죽었을 때 호출할 함수
+    public void PlayerDied()
     {
-        
+        isPlayerDead = true;
+        // 추가적으로 게임 종료 처리나 사망 애니메이션 등을 구현할 수 있습니다.
+    }
+
+    // 예시: 플레이어가 부활할 때 호출할 함수
+    public void PlayerRevived()
+    {
+        isPlayerDead = false;
+        // 추가적으로 부활 처리나 애니메이션 등을 구현할 수 있습니다.
     }
 }
